c8ae8491e6069a2eac8ca3f9e05648e2fc73b94f
Brian Gouws <briangouws@hotmail.com>
2018-02-01 10:14:16 +0000

    added restful services request method to api

--- microservice-a/common/models/consul.js
+++ microservice-a/common/models/consul.js
@@ -1,21 +1,41 @@
-'use strict';
+'use strict'
 
-module.exports = function(Consul) {
-  Consul.status = function(cb) {
+module.exports = function (Consul) {
+  Consul.status = cb => {
     var date = new Date()
     var response = date.getTime()
-    cb(null, response);
-  };
-  Consul.remoteMethod(
-    'status', {
-      http: {
-        path: '/healthcheck',
-        verb: 'get'
-      },
-      returns: {
-        arg: 'status',
-        type: 'int'
-      }
+    cb(null, response)
+  }
+  Consul.remoteMethod('status', {
+    http: {
+      path: '/healthcheck',
+      verb: 'get'
+    },
+    returns: {
+      arg: 'status',
+      type: 'int'
     }
-  );
-};
+  })
+  Consul.test = cb => {
+    // get service through rest datasource
+    // let services = Consul.services()
+    // services
+    //   .then(response => {
+    //     cb(null, response)
+    //   })
+    //   .catch(error => {
+    //     cb(null, error)
+    //   })
+  }
+  Consul.remoteMethod('services', {
+    http: {
+      path: '/services',
+      accepts: { arg: 'serviceName' },
+      verb: 'get'
+    },
+    returns: {
+      arg: 'test',
+      type: 'string'
+    }
+  })
+}
